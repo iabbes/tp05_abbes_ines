34b64e2c45915765416456a6692a496a0e0dc830
Ines_PC_windows <Ines_PC_windows@gmail.com>
2022-11-24 20:23:32 +0100

    aaa

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-accueil',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+  isConnected = false
 
-  constructor() { }
+  constructor(private loginService: LoginService,private router:Router) { 
+    
+  }
 
   ngOnInit(): void {
   }
-
+  onSubmit() {
+    this.loginService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe(
+      (data) => {
+        this.isConnected = true;
+        this.router.navigateByUrl('avion/listPlane');
+      }
+    );
+  }
 }
